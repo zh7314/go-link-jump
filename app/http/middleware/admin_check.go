b50e5246d80ea5d07f6aa60128d43c04bf1dcd33
Z.X <903464207@qq.com>
2023-05-19 10:34:05 +0800

    1

--- app/http/middleware/admin_check.go
+++ app/http/middleware/admin_check.go
@@ -1,7 +1,9 @@
 package middleware
 
 import (
+	"errors"
 	"github.com/goravel/framework/contracts/http"
+	"goravel/app/utils/global"
 )
 
 func AdminCheck() http.Middleware {
@@ -9,6 +11,31 @@ func AdminCheck() http.Middleware {
 
 		//ctx.WithValue("user", "Goravel")
 
+		//method := ctx.Request().Method()
+
+		//var token string
+
+		//if method == http.MethodPost {
+		//
+		//
+		//} else if method == http.MethodGet {
+		//
+		//} else {
+		//	fmt.Println(method)
+		//}
+
 		ctx.Request().Next()
 	}
+}
+func check(ctx http.Context) (res bool, ok error) {
+	token := ctx.Request().Header(global.API_TOKEN, "")
+	if token == "" {
+		token = ctx.Request().Input(global.API_TOKEN)
+	}
+
+	if token == "" {
+		return false, errors.New("token不能为空")
+	}
+
+	return true, nil
 }
